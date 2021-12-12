8fd95b3a4c6bdb1faf57e72388b5a0be8bab4f73
Selam Alem <selamtalem@gmail.com>
2021-12-11 18:26:03 -0600

    Completed getting genres

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 
+
 function MovieDetail() {
     const history = useHistory();
+    const dispatch = useDispatch();
+  
+
+    useEffect(() => {
+        dispatch({
+            type: 'SET_DETAIL',
+           
+        })
+    }, [])
 
     const movieDetail = useSelector(store => store.selectedMovie);
     console.log('movie detail:', movieDetail);
@@ -33,8 +44,14 @@ function MovieDetail() {
         </div>
 
         <div>
+       
         {movieDetail[0].description}
-        {movieDetail[0].genres}
+     
+        {movieDetail[0].array_agg.map((detail) => {
+            return <li> {detail} </li>
+        })}
+    
+        
         </div>
         <button onClick={handleBackToListButton}>Back To List</button>
     </div>
